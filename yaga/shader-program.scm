4a5ac38aff9431f98952bdb980f182b0da3a95ff
Aeva Palecek <aeva@lunarfracking.com>
2017-05-21 14:28:23 -0500

    gather-program-vars can now traverse into records
    The gather-program-vars method can now traverse into records, and read
    local references to other fields in that record.
    
    This is almost complete, with these remaining cases:
    
     1. Passing a binding name into a function call.
    
     2. Invoking a local reference as a function call.
    
     3. Aliases, via let or define.
    
    The first two can probably be (badly?) approximated by some variety of
    look-ahead mechanism.
    
    With the current shader.scm example, this code as-is only can give us
    the following:
    
    (<model> screen-position)
    (<model> world-matrix)
    (<model> vertex-position)
    (<model> texture-map)
    (<model> vertex-uv))
    
    What is absent is the <camera> references, which should have resulted
    from (<model> screen-position).
    
    But also, this scheme totally does not account for arrays of models, and
    I'm not yet sure how to account for that.
    
    It feels like this approach may be a dead-end, and that instead this
    should be replaced with some kind of variant on a proper meta-circular
    evaluator.

--- yaga/shader-program.scm
+++ yaga/shader-program.scm
@@ -54,33 +54,72 @@
     (make-environment types shaders programs)))
 
 
-;;
+;; Produce a list of all unique record lookups used in a given shader
+;; program.
 (define (gather-program-vars program env)
 
-  ;;
+  ;; For a given named type and named field, return the value of that
+  ;; field, as well as a context object for the named type record.
+  (define (open-ref type-name field-name)
+    (let* ([type-def (fetch type-name (environment-types env))]
+           [clear (lambda (pair) (cons (car pair) '()))]
+           [buffers (map clear (fetch 'buffers type-def))]
+           [controls (map clear (fetch 'controls type-def))]
+           [functions (fetch 'functions type-def)]
+           [local-vars (append buffers controls functions)])
+      (cons (fetch field-name local-vars) (cons type-name local-vars))))
+
+  ;; Determine if the given expression is a valid record lookup,
+  ;; according to the current bindings.
   (define (is-lookup? expr bindings)
     (cond [(null? expr) #f]
           [(null? bindings) #f]
           [(pair? expr) (assoc (car expr) bindings)]
           [else #f]))
 
-  ;;
-  (define (traverse expr bindings)
+  ;; Traverse an execution path and return all unique referenced
+  ;; compound type records.  This can recurse into records.
+  (define (traverse expr bindings context)
     (cond
-     [(or (null? expr)(not (pair? expr))) '()]
+     [(null? expr) '()]
+     [(and (not (pair? expr)) (null? context)) '()]
+     [(not (pair? expr))
+      ;; We have an environment type as the context, so see if the
+      ;; atom is a local reference and return or recurse accordingly.
+      (let* ([type-name (car context)]
+             [all-vars (cdr context)]
+             [found (fetch expr all-vars)])
+        (cond
+         ;; If the atom is a field in the context, either return just
+         ;; the reference id, or the reference id and recurse into the
+         ;; next expression if applicable.
+         [(not found) '()]
+         [(null? found) (list (list type-name expr))]
+         [else (traverse found '() context)]))]
      [(pair? expr)
+      ;; TODO - this path currently does not account for if the pair
+      ;; in question is a function call, all it does is check to see
+      ;; if the sexpr is a record lookup.
       (let ([found (is-lookup? expr bindings)])
         (cond
-         [found (list (list (cadr found) (cadr expr)))]
+         [found (let* ([type-name (cadr found)]
+                       [field-name (cadr expr)]
+                       [found (list (list type-name field-name))]
+                       [inspect (open-ref type-name field-name)]
+                       [ref-expr (car inspect)]
+                       [new-context (cdr inspect)])
+                  (cond
+                   [(null? ref-expr) found]
+                   [else (append found (traverse ref-expr '() new-context))]))]
          [else
-          (let ([recurse (lambda (nexpr) (traverse nexpr bindings))])
+          (let ([recurse (lambda (nexpr) (traverse nexpr bindings context))])
             (apply append (map recurse expr)))]))]))
   
-  (newline)(newline)
   (let* ([shader-names (map cadr (car (fetch 'shaders program)))]
          [shaders (map (lambda (name) (lookup-shader name env)) shader-names)]
          [inputs (fetch 'inputs program)]
          [transports (fetch 'transports program)]
          [bindings (append inputs transports)]
-         [inspect (lambda (shader) (traverse (fetch 'body (cdr shader)) bindings))])
+         [inspect
+          (lambda (shader) (traverse (fetch 'body (cdr shader)) bindings '()))])
     (delete-duplicates (apply append (map inspect shaders)))))
