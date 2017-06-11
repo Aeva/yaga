e4842a6b4a46a26fd7afde40807cd72bed09ccc8
Aeva Palecek <aeva@lunarfracking.com>
2017-06-11 13:10:05 -0500

    Adding documentation to type-inference methods.

--- yaga/common.scm
+++ yaga/common.scm
@@ -1,16 +1,33 @@
 
 (define-module (yaga common)
-  #:export (fetch keyword->string))
-
+  #:export (fetch keyword->string limit range))
 
 ;; Similar to (assoc key alist), but only returns the matching value.
 (define (fetch key alist)
   (let ([found (assoc key alist)])
     (if found (cdr found) #f)))
 
-
 ;; Guile doesn't define this.  Weird.
 (define (keyword->string keyword)
   (if (keyword? keyword)
       (symbol->string (keyword->symbol keyword))
       ""))
+
+;; Return a subset of an input list that matches the matcher function.
+(define (limit matcher list)
+  (define matched '())
+  (for-each
+   (lambda (item) (if (matcher item) (set! matched (cons item matched))))
+   list)
+  matched)
+
+;; Analagous to python's range builtin.  Eg, (range 4) --> (0 1 2 3)
+(define (range count)
+  (define (build-range count acc)
+    (cond [(zero? count) acc]
+          [else (let ([next (- count 1)])
+                  (build-range next (cons next acc)))]))
+  (cond
+   [(and (number? count) (>= count 0))
+    (build-range count '())]
+   [else (error "Range input must be a number greater than or equal to zero")]))
--- yaga/type-inference/patterns.scm
+++ yaga/type-inference/patterns.scm
@@ -1,27 +1,15 @@
 (define-module (yaga type-inference patterns)
+  #:use-module (yaga common)
   #:use-module (srfi srfi-1)
   #:export (solve permutate multi-permutate))
 
-
-(define (limit matcher list)
-  (define matched '())
-  (for-each
-   (lambda (item) (if (matcher item) (set! matched (cons item matched))))
-   list)
-  matched)
-
-
-(define (range count)
-  (define (build-range count acc)
-    (cond [(zero? count) acc]
-          [else (let ([next (- count 1)])
-                  (build-range next (cons next acc)))]))
-  (cond
-   [(and (number? count) (>= count 0))
-    (build-range count '())]
-   [else (error "Range input must be a number greater than or equal to zero")]))
-  
-
+;; "Solved-portion" is a helper function to "solve".  The
+;; "permutations" input is a list of types that have been determined
+;; to be valid subsets of the full permutation matrix for the
+;; currently known inputs.  This function then scans that possibility
+;; space, and notes which columns all contain the same type.  This
+;; function then returns a list where each slot is either null or a
+;; locked type.
 (define (solved-portion permutations)
   (define slots (range (length (car permutations))))
   (define (same-for-slot? index)
@@ -32,7 +20,24 @@
         '()))
   (map same-for-slot? slots))
 
-
+;; "Solve" takes a matrix of permutations (or, permutatrix) and a list
+;; of types associated to the drains of a function (or null, if the
+;; drain's type is unknown).  The first drain represent the function's
+;; output, and the remaining drains are the ordered inputs.  So a call
+;; might be:
+;;
+;;   (solve '((#:float #:float)
+;;            (#:float #:float2)
+;;            (#:float #:float3)
+;;            (#:float #:float4)) '() #:float4)
+;;
+;; ...and the output is a new list of types.  So in the example above,
+;; the output would be:
+;;
+;;   '(#:float #:float4)
+;;
+;; If a contradiction is discovered, then a type error will be thrown
+;; from this function.
 (define (solve permutatrix . drain-types)
   (define (validate-permutation? permute)
     (define (type-matches-pattern? drain-type pattern-type)
@@ -51,7 +56,36 @@
   (cond [(null? variations) '()]
         [else (solved-portion variations)]))
 
-
+;; "Permutate" generates a permutation matrix for the "solve" method
+;; to consume.  It recognizes two virtual types in addition to all
+;; primitive types: #:vector and #:matrix.  These virtual types are
+;; respectively standins for:
+;;
+;;   (#:float #:float2 #:float3 #:float4)
+;;
+;; and,
+;;
+;;   (#:float #:matrix2 #:matrix3 #:matrix4)
+;;
+;; These virtual types are roughly the same idea as OpenGL's various
+;; "GenType" keywords used for simplifying the valid type signatures
+;; of builtin methods in the spec.  In the future, the meanings of
+;; #:vector and #:matrix might be expanded to circumstantially include
+;; non-float types when the output language supports it.  In this
+;; event, additional types like #:float-vector will be introduced.
+;;
+;; But, the future aside, a call to "permutate" looks something like
+;; this:
+;;
+;;   (permutate '(#:float #:vector))
+;;
+;; and returns something like this:
+;;
+;;   '((#:float #:float)
+;;     (#:float #:float2)
+;;     (#:float #:float3)
+;;     (#:float #:float4))
+;;
 (define (permutate pattern)
   (define sizes
     (if 
@@ -68,27 +102,15 @@
     (map subst pattern))
   (map permute (range sizes)))
 
-
+;; "Multi-permutate" is a convienience method on top of "permutate",
+;; where you provide a list of permutations you want to contactenate
+;; togther, and it returns a single permutation matrix with no
+;; duplicates.  For example, binary types that only accept operands of
+;; the same type would look like this:
+;;
+;;   (multi-permutate '((#:float #:float)
+;;                      (#:vector #:vector)
+;;                      (#:matrix #:matrix)))
+;;
 (define (multi-permutate rows)
   (delete-duplicates (apply append (map permutate rows))))
-
-
-;; (define (*-solve out lhs rhs)
-;;   (define permutatrix
-;;     ;; I later looked up the rules for this, and this isn't actually
-;;     ;; the correct permutation matrix for the GLSL * operator, but it
-;;     ;; is good enough for example porpoises.
-    
-;;     ;; output input input
-;;     '((#:number #:number #:number)
-;;       (#:vector #:vector #:number)
-;;       (#:vector #:vector #:vector)
-;;       (#:vector #:matrix #:vector)
-;;       (#:matrix #:matrix #:matrix)))
-;;   (solve permutatrix out lhs rhs))
-
-;; (display (*-solve '() '() '())) (newline)
-;; (display (*-solve #:number '() '())) (newline)
-;; (display (*-solve '() #:vector '())) (newline)
-;; (display (*-solve '() '() #:matrix)) (newline)
-;; ;;(display (*-solve #:number '() #:matrix)) (newline) ; will be type error
